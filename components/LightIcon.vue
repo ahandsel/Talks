0f83f2dedecea4e55f8321f9f1aa3dd9695c3f03
ahandsel <genji-fujimori@cybozu.co.jp>
2022-05-16 11:34:25 +0900

    Imported theme code from light-icons theme

---
+++ components/LightIcon.vue
@@ -0,0 +1,21 @@
+<template>
+    <i :class="`light-icon-${icon}`" :style="`font-size: ${size}; color: ${color}; vertical-align: middle;`"/>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+const props = defineProps({
+    icon: {
+        type: String,
+        default: 'camera'
+    },
+    size: {
+        type: String,
+        default: 'inherit'
+    },
+    color: {
+        type: String,
+        default: 'inherit'
+    },
+});
+</script>
---
+++ layouts/image-left.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="relative h-full intro grid grid-cols-12">
+    <div class="absolute h-full w-full" style="z-index: -10">
+      <BubbleFrame v-if="!hideFrame"/>
+    </div>
+    <div
+      v-if="image"
+      :class="[
+        equal ? 'col-span-6' : 'col-span-4',
+        'my-auto h-full flex justify-center items-center',
+      ]"
+    >
+      <img
+        :src="image"
+        class="my-auto mx-auto w-auto"
+        :style="{
+          maxHeight: '70%',
+        }"
+      />
+    </div>
+    <div
+      :class="[
+        'slidev-layout my-auto',
+        image ? 'col-span-6' : 'col-span-12',
+        equal ? 'col-span-6' : 'col-span-8',
+      ]"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Dependencies
+import { defineProps } from "vue";
+
+const props = defineProps({
+  image: {
+    type: String,
+    required: false,
+  },
+  equal: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+  hideFrame: {
+    type: Boolean,
+    default: false,
+  }
+});
+</script>
