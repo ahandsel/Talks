b15476b28df36e9ef73a5dd036324e47f11df464
ahandsel <genji-fujimori@cybozu.co.jp>
2022-06-08 19:31:00 +0900

    duplicate from image-left-bar

---
+++ layouts/image-right-bar.vue
@@ -0,0 +1,49 @@
+<!-- my-image-left -->
+<template>
+  <div class="relative h-full intro grid grid-cols-12">
+    <div class="absolute h-full w-full" style="z-index: -10">
+      <BubbleFrame v-if="!hideFrame" />
+    </div>
+
+    <!-- Image -->
+    <div :class="[equal ? 'col-span-6' : 'col-span-4', 'my-auto h-full']">
+      <div class="my-auto h-full" :style="{ ...style }"></div>
+    </div>
+    <!-- Layout -->
+    <div :class="[
+      'slidev-layout my-auto',
+      image ? 'col-span-6' : 'col-span-12',
+      equal ? 'col-span-6' : 'col-span-8',
+    ]">
+      <slot /> <!-- Grab Markdown SliDev content -->
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Dependencies
+import { computed, defineProps } from "vue";
+
+// Utils
+import { handleBackground } from "../utils/index";
+
+const props = defineProps({
+  image: {
+    type: String,
+    required: true,
+  },
+  equal: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+  hideFrame: {
+    type: Boolean,
+    default: false,
+  }
+});
+
+const style = computed(() =>
+  handleBackground(props.image)
+);
+</script>
